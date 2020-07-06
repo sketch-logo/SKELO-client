3f10e4389f99753ab15aa06c856a2400c0324cdc
TorryTheMaltese <torryiscute@gmail.com>
2020-07-06 18:48:31 +0900

    Add drawing-selecting-mode-switching button

--- src/components/CanvasArea.vue
+++ src/components/CanvasArea.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="cvs-container">
+    <div class="cvs_tools">
+      <div class="tool_btn" @click="onClickPointerBtn">
+        <font-awesome-icon icon="arrows-alt" class="tool_icon"></font-awesome-icon>
+      </div>
+      <div class="tool_btn" @click="onClickDrawingBtn">
+        <font-awesome-icon icon="pen" class="tool_icon"></font-awesome-icon>
+      </div>
+    </div>
     <canvas class="cvs" id="cvs" ref="cvs"></canvas>
   </div>
 </template>
@@ -7,25 +15,36 @@
 <script>
 import { fabric } from "fabric";
 
+import { library as faLibrary } from "@fortawesome/fontawesome-svg-core";
+import { faArrowsAlt, faPen } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+faLibrary.add(faArrowsAlt, faPen);
+
 export default {
+  data() {
+    return {
+      canvas: ""
+    };
+  },
+  components: {
+    FontAwesomeIcon
+  },
   mounted() {
     const ref = this.$refs.cvs;
-    const canvas = new fabric.Canvas(ref, { isDrawingMode: true });
+    this.canvas = new fabric.Canvas(ref, { isDrawingMode: true });
 
-    canvas.setHeight(500);
-    canvas.setWidth(500);
-    canvas.on("mouse:up", function() {
-      canvas.isDrawingMode = false;
-    });
-
-    const rect = new fabric.Rect({
-      fill: "red",
-      width: 20,
-      height: 20
-    });
-    canvas.add(rect);
+    this.canvas.setHeight(500);
+    this.canvas.setWidth(500);
   },
-  methods: {}
+  methods: {
+    onClickPointerBtn() {
+      this.canvas.isDrawingMode = false;
+    },
+    onClickDrawingBtn() {
+      this.canvas.isDrawingMode = true;
+    }
+  }
 };
 </script>
 
@@ -35,6 +54,49 @@ export default {
   display: flex;
   justify-content: center;
 }
+
+/*  Canvas Tools  *****************************************************************/
+
+.cvs_tools {
+  display: flex;
+  flex-direction: column;
+}
+.cvs_tools .tool_btn {
+  width: 60px;
+  height: 60px;
+  position: relative;
+  display: flex;
+  margin: 20px;
+  border-radius: 100%;
+  cursor: pointer;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+  background: $white;
+  transition: all 0.1s ease-in-out;
+  box-shadow: -6px -6px 10px rgba(255, 255, 255, 0.8),
+    6px 6px 10px rgba(0, 0, 0, 0.2);
+
+  &:hover {
+    opacity: 0.3;
+    box-shadow: -6px -6px 10px rgba(255, 255, 255, 0.8),
+      6px 6px 10px rgba(0, 0, 0, 0.2);
+  }
+
+  &:active {
+    opacity: 1;
+    box-shadow: inset -4px -4px 8px rgba(255, 255, 255, 0.5),
+      inset 8px 8px 16px rgba(0, 0, 0, 0.1);
+  }
+}
+
+.cvs_tools .tool_icon {
+  width: 30px;
+  height: 30px;
+}
+
+/*  Canvas  *****************************************************************/
 
 .cvs {
   background-color: white;
